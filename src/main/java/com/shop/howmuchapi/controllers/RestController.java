4b5364211667910fd96a264877e62a40288ac49f
grigorii <grigoriiborodin@yandex.ru>
2022-07-20 17:24:50 +0300

    code clean-up

--- src/main/java/com/shop/howmuchapi/controllers/RestController.java
+++ src/main/java/com/shop/howmuchapi/controllers/RestController.java
@@ -4,23 +4,22 @@ import com.shop.howmuchapi.dto.CalculatedCart;
 import com.shop.howmuchapi.dto.Cart;
 import com.shop.howmuchapi.services.HowMuchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/carts")
-public class HowMuchController {
+public class RestController {
     private HowMuchService serviceLayer;
 
-    public HowMuchController(
+    public RestController(
         @Autowired final HowMuchService serviceLayer
     ) {
         this.serviceLayer = serviceLayer;
     }
 
-    @PutMapping()
+    @PostMapping()
     public CalculatedCart calculateCart (@RequestBody final Cart cart) {
         return serviceLayer.getCalculatedCart(cart);
     }
